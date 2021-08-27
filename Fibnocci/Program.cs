b96d1ba790c6f68b029854ff4b480a182d31d6ee
Vimal Raveendran <vimalraveendra@gmail.com>
2021-08-27 21:01:20 +0200

    initial commit

---
+++ Fibnocci/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Fibnocci
+{
+   
+    class Program
+    {
+
+        public static int Fibonacci(int n)
+        {
+            int a = 0;
+            int b = 1;
+            // In N steps compute Fibonacci sequence iteratively.
+            for (int i = 0; i < n; i++)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a;
+        }
+
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Enter the length of the fibinocci:");
+            int len = Convert.ToInt32(Console.ReadLine());
+           
+            for (int i = 0; i < len; i++)
+            {
+                await Task.Delay(3000); 
+                Console.WriteLine(Fibonacci(i));
+                
+            }
+        }
+
+        
+    }
+}
